c5487f709590f91329f6cc333253269d34235f0d
yurianisimov27 <yurianisimov27@gmail.com>
2020-06-30 22:41:00 +0300

    masret commit 2

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -127,7 +127,7 @@ function repeatString(value, count) {
  * @example
  *   'To be or not to be', 'not'  => 'To be or to be'
  *   'I like legends', 'end' => 'I like legs',
- *   'ABABAB','BA' => 'ABAB'
+ *   'ABABAB','BA' => 'ABAB'q
  */
 function removeFirstOccurrences(str, value) {
     throw new Error('Not implemented');
