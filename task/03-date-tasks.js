f3893b0d17776a9a9753b76b837d586873930a5a
yurianisimov27 <yurianisimov27@gmail.com>
2020-07-01 17:38:00 +0300

    solution task 3

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -81,10 +81,8 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-    return (new Date(endDate - startDate).toISOString().slice(11, -1));
-   throw new Error('Not implemented');
+    return new Date(endDate - startDate).toISOString().slice(11, -1)
 }
-
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock for the specified Greenwich time.
